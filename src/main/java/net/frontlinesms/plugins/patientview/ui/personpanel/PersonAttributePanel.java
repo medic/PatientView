eb6c4e23c9e251599679494de76f2c62f9d47c6b
Dieterich Lawson <dieterich.lawson@gmail.com>
2011-05-25 15:33:52 -0500

    Fixed attribute bug
    Attribute editing controls now show up
    when the user clicks 'edit profile'.

--- src/main/java/net/frontlinesms/plugins/patientview/ui/personpanel/PersonAttributePanel.java
+++ src/main/java/net/frontlinesms/plugins/patientview/ui/personpanel/PersonAttributePanel.java
@@ -24,6 +24,9 @@ import org.springframework.context.ApplicationContext;
 
 import thinlet.Thinlet;
 
+/**
+ * This class handles the display of person attributes in the detail view.
+ */
 public class PersonAttributePanel {
 	
 	
@@ -50,16 +53,19 @@ public class PersonAttributePanel {
 		this.appCon = appCon;
 		this.person = person;
 		this.fieldGroup = new FieldGroup(uiController, appCon, null);
+		// create the main ui objects
 		mainPanel = Thinlet.create("panel");
 		uiController.setInteger(mainPanel,"weightx",1);
 		//uiController.setInteger(mainPanel,"weighty",1);
 		uiController.setInteger(mainPanel, "columns", 1);
 		uiController.setInteger(mainPanel, "left", 5);
 		uiController.setGap(mainPanel, 10);
+		//fetch the DAOs
 		attributeDao = (PersonAttributeDao) appCon.getBean("PersonAttributeDao");
 		fieldDao = (MedicFormFieldDao) appCon.getBean("MedicFormFieldDao");
 		attributeResponseDao = (PersonAttributeResponseDao) appCon.getBean("PersonAttributeResponseDao");
 		fieldResponseDao = (MedicFormFieldResponseDao) appCon.getBean("MedicFormFieldResponseDao");
+		//do the main initialization
 		initPanel();
 	}
 	
@@ -72,73 +78,69 @@ public class PersonAttributePanel {
 		inEditingMode = false;
 		uiController.removeAll(mainPanel);
 		for(PersonAttribute attribute: attributeDao.getAnsweredAttributesForPerson(person)){
-			String value = "";
-			PersonAttributeResponse response= attributeResponseDao.getMostRecentAttributeResponse(attribute, person);
+			PersonAttributeResponse response = attributeResponseDao.getMostRecentAttributeResponse(attribute, person);
 			DataType type = attribute.getDatatype();
 			if(type.isBoolean()){
-				value = (response.getValue().equalsIgnoreCase("true")) ? type.getTrueLabel() :type.getFalseLabel();
+				String value = response.getValue().equalsIgnoreCase(InternationalisationUtils.getI18nString("datatype.true"))? type.getTrueLabel() :type.getFalseLabel();
+				uiController.add(mainPanel,getFieldDisplayPanel(value,attribute.getLabel()));
 			}else if(type == DataType.TEXT_AREA){
-				Object textArea = Thinlet.create("textarea");
-				uiController.setInteger(textArea, "weightx",1);
-				Object panel = Thinlet.create("panel");
-				uiController.setInteger(panel,"columns",1);
-				uiController.setInteger(panel, "gap", 4);
-				uiController.setWeight(panel, 1, 0);
-				uiController.setColspan(panel, 1);
-				uiController.setEditable(textArea,false);
-				uiController.setText(textArea, response.getValue());
-				Object label = uiController.createLabel(attribute.getLabel());
-				uiController.add(panel,label);
-				uiController.add(panel,textArea);
-				uiController.add(mainPanel,panel);
+				uiController.add(mainPanel,getTextAreaDisplayPanel(response.getValue(), attribute.getLabel()));
 			}else{
-				value = response.getValue();
-			}
-			if(type != DataType.TEXT_AREA){
-				Object item = uiController.createLabel(attribute.getLabel() +": "+ value);
-				uiController.setColspan(item, 1);
-				uiController.add(mainPanel,item);
+				uiController.add(mainPanel,getFieldDisplayPanel(response.getValue(),attribute.getLabel()));
 			}
 		}
 		if(person instanceof Patient){
 			for(MedicFormField field: fieldDao.getAnsweredAttributePanelFieldsForPerson(person)){
-				String value = "";
-				MedicFormFieldResponse response= fieldResponseDao.getMostRecentFieldResponse(field, person);
+				MedicFormFieldResponse response = fieldResponseDao.getMostRecentFieldResponse(field, person);
 				DataType type = field.getDatatype();
 				if(type.isBoolean()){
-					value = response.getValue().equalsIgnoreCase(InternationalisationUtils.getI18nString("datatype.true"))? type.getTrueLabel() :type.getFalseLabel();
+					String value = response.getValue().equalsIgnoreCase(InternationalisationUtils.getI18nString("datatype.true"))? type.getTrueLabel() :type.getFalseLabel();
+					uiController.add(mainPanel,getFieldDisplayPanel(value,field.getLabel()));
 				}else if(type == DataType.TEXT_AREA){
-					Object textArea = Thinlet.create("textarea");
-					uiController.setInteger(textArea, "weightx",1);
-					Object panel = Thinlet.create("panel");
-					uiController.setInteger(panel,"columns",1);
-					uiController.setWeight(panel, 1, 0);
-					uiController.setGap(panel, 4);
-					uiController.setEditable(textArea,false);
-					uiController.setText(textArea, response.getValue());
-					uiController.setColspan(panel, 1);
-					Object label = uiController.createLabel(field.getLabel());
-					uiController.add(panel,label);
-					uiController.add(panel,textArea);
-					uiController.add(mainPanel,panel);
+					uiController.add(mainPanel,getTextAreaDisplayPanel(response.getValue(), field.getLabel()));
 				}else{
-					value = response.getValue();
-				}
-				if(type != DataType.TEXT_AREA){
-					Object item = uiController.createLabel(field.getLabel() +": "+ value);
-					uiController.setColspan(item, 1);
-					uiController.add(mainPanel,item);
+					uiController.add(mainPanel,getFieldDisplayPanel(response.getValue(),field.getLabel()));
 				}
 			}
 		}
 	}
 	
 	/**
+	 * Returns a thinlet panel that displays the response
+	 * to a text area field
+	 */
+	private Object getTextAreaDisplayPanel(String value, String label){
+		Object textArea = Thinlet.create("textarea");
+		uiController.setInteger(textArea, "weightx",1);
+		Object panel = Thinlet.create("panel");
+		uiController.setInteger(panel,"columns",1);
+		uiController.setWeight(panel, 1, 0);
+		uiController.setGap(panel, 4);
+		uiController.setEditable(textArea,false);
+		uiController.setText(textArea, value);
+		uiController.setColspan(panel, 1);
+		Object fLabel = uiController.createLabel(label);
+		uiController.add(panel,fLabel);
+		uiController.add(panel,textArea);
+		return panel;
+	}
+	
+	/**
+	 * Returns a thinlet panel that displays the response
+	 * to any field except a text area field
+	 */
+	private Object getFieldDisplayPanel(String value, String label){
+		Object panel = uiController.createLabel(label +": "+ value);
+		uiController.setColspan(panel, 1);
+		return panel;
+	}
+	/**
 	 * Removes whatever was previously in the panel and 
 	 * adds editable fields for every attribute that the
 	 * person could have.
 	 */
 	public void switchToEditingPanel(){
+		uiController.removeAll(mainPanel);
 		fieldGroup.removeAll();
 		ArrayList<Field> attributes = new ArrayList<Field>();
 		attributes.addAll(attributeDao.getAllAttributesForPerson(person));
@@ -160,6 +162,7 @@ public class PersonAttributePanel {
 				}
 			}
 			fieldGroup.addField(tff);	
+			uiController.add(mainPanel,fieldGroup.getMainPanel());
 		}
 		inEditingMode=true;
 	}
